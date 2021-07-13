65051b28cfa2c34d1ab0e2cbd3a482b2250524f1
Alexander Getmansky <alexander.get87@gmail.com>
2021-07-13 21:31:40 +0300

    feat (health): Add health indicator for characters

---
+++ Assets/Scripts/Character/Component/HealthIndicator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Character.Component
+{
+    public class HealthIndicator : MonoBehaviour
+    {
+        private TextMesh textMesh;
+        private HealthComponent healthComponent;
+
+        private void Start()
+        {
+            textMesh = GetComponent<TextMesh>();
+            healthComponent = GetComponentInParent<HealthComponent>();
+            IndicatorUpdate(healthComponent.health);
+            healthComponent.OnHealthChanged += IndicatorUpdate;
+        }
+
+        private void IndicatorUpdate(int value)
+        {
+            textMesh.text = value > 0 ? value.ToString() : "";
+        }
+    }
+}
