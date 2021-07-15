83c04fb749d323f27f52d0ce391ab8a303e77ef1
Alexander Getmansky <alexander.get87@gmail.com>
2021-07-15 18:09:24 +0300

    refactor (animations): Clear code

--- Assets/Scripts/Character/Component/DamageEffect.cs
+++ Assets/Scripts/Character/Component/DamageEffect.cs
@@ -7,15 +7,6 @@ namespace Character.Component
     {
         public GameObject damageEffect;
 
-        // todo: Нужно спросить, как сделать так чтобы не проигрывались эффекты при старте уровня.
-        public void Start()
-        {
-            foreach (var effect in damageEffect.GetComponentsInChildren<ParticleSystem>())
-            {
-                effect.Stop();
-            }          
-        }
-
         public void ShowDamageEffect()
         {
             foreach (var effect in damageEffect.GetComponentsInChildren<ParticleSystem>())
